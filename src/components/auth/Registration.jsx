a77ca35db5be1105f2f50d286119f0d067db2731
mrartur0074 <smiteartur01@mail.ru>
2023-02-14 11:26:27 +0600

    add valide

--- src/components/auth/Registration.jsx
+++ src/components/auth/Registration.jsx
@@ -1,28 +1,105 @@
+import {useForm} from "react-hook-form";
+
 const Registration = (props) => {
+
+    const {register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const onSubmit = async data => {
+        console.log(data)
+    }
+
+    console.log(errors)
 
     return (
         <div className="form-modal">
             <div className="title-modal">Регистрация</div>
-            <form>
-                <div>
-                    <label htmlFor="login"></label>
-                    <input type="text" name="login" placeholder="Введите логин"/>
+            <form className="modal-form" onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-input">
+                    <label htmlFor="firstName">Имя:</label>
+                    <input 
+                        type="text" 
+                        name="firstName" 
+                        placeholder="Введите ваше имя" 
+                        {...register('firstName', {
+                            required: "Параметр обязателен", 
+                            maxLength: {
+                                value: 20,
+                                message: 'Ваше имя должно быть меньше 20 символов'
+                            }, 
+                            minLength: {
+                                value: 3,
+                                message: 'Ваше имя должно быть больше 3 символов'
+                            } 
+                        })}
+                    />
+                    {errors.firstName && <span className="error" role="alert">{errors.firstName?.message}</span>}
                 </div>
-                <div>
-                    <label htmlFor="email"></label>
-                    <input type="text" name="email" placeholder="Введите почту"/>
+                <div className="form-input">
+                    <label htmlFor="email">Email: </label>
+                    <input 
+                        type="text" 
+                        name="email" 
+                        placeholder="Введите почту"
+                        {...register("email", {
+                            required: "Параметр обязателен",
+                            pattern: {
+                              value: /\S+@\S+\.\S+/,
+                              message: "Ваш email не подходит под нужный формат"
+                            }
+                        })}
+                    />
+                    {errors.email && <span className="error" role="alert">{errors.email?.message}</span>}
                 </div>
-                <div>
-                    <label htmlFor="firstName"></label>
-                    <input type="text" name="firstName" placeholder="Введите ваше имя"/>
+                <div className="form-input">
+                    <label htmlFor="login">Login: </label>
+                    <input 
+                        type="text" 
+                        name="login" 
+                        placeholder="Введите логин"
+                        {...register("login", {
+                            required: "Параметр обязателен",
+                            pattern: {
+                              value: /[A-Za-z]/,
+                              message: "Логин должен содержать только латинские символы"
+                            },
+                            maxLength: 20, 
+                            minLength: 3
+                        })}
+                    />
+                    {errors.login && <span className="error" role="alert">{errors.login?.message}</span>}
                 </div>
-                <div>
-                    <label htmlFor="password"></label>
-                    <input type="password" name="password" placeholder="Введите пароль"/>
+                <div className="form-input">
+                    <label htmlFor="password">Пароль: </label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Введите пароль"
+                        {...register("password", {
+                            required: "Параметр обязателен",
+                            minLength: {
+                              value: 5,
+                              message: "Минимальная длина пароля 5 символов"
+                            }
+                          })}
+                    />
+                    {errors.password && <span className="error" role="alert">{errors.password?.message}</span>}
                 </div>
-                <div>
-                    <label htmlFor="passwordRepeat"></label>
-                    <input type="password" name="passwordRepeat" placeholder="Повторите пароль"/>
+                <div className="form-input">
+                    <label htmlFor="cpassword">Повторите пароль: </label>
+                    <input 
+                        type="password" 
+                        name="cpassword" 
+                        placeholder="Повторите пароль"
+                        {...register("cpassword", {
+                            validate: (value) => {
+                                if (watch('password') != value) {
+                                    return "Ваши пароли не совпадают";
+                                }
+                            },
+                            required: 'Параметр обязателен'
+                        })}
+                    />
+                    {errors.cpassword && <span className="error" role="alert">{errors.cpassword?.message}</span>}
                 </div>
                 <div>
                     <label htmlFor="submit"></label>
